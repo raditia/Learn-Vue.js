c6c76c772e18e83a7ea4ec65f560efe822e6b32b
Raditia Madya <gustiraditia@gmail.com>
2018-01-11 13:22:01 +0700

    Add api

--- src/pages/LearnAPI.vue
+++ src/pages/LearnAPI.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
       Hey, I'm API!
+      <button @click="callApi">Call API</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'LearnAPI'
+    name: 'LearnAPI',
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+        callApi: function () {
+            const URL = 'https://ghibliapi.herokuapp.com/films'
+
+            //call api
+            axios.get(URL)
+            .then(response => {
+                console.log('Hi', response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            console.log('call api with ', axios)
+        }
+    }
   
 }
 </script>
